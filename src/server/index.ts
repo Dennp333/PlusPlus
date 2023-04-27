8d1376ac8217d2912837c408d90f723c3f0a89e6
Dennis Peng <dennp333@gmail.com>
2023-04-26 23:10:43 -0400

    server cleanup

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,27 +1,14 @@
 import {
   onOpen,
   openDialog,
-  openDialogBootstrap,
-  openDialogMUI,
-  openDialogTailwindCSS,
-  openAboutSidebar,
 } from './ui';
 
-import { getSheetsData, addSheet, deleteSheet, setActiveSheet } from './sheets';
 import { getSelectedText, insertOrReplaceText } from './docs';
 
 // Public functions must be exported as named exports
 export {
   onOpen,
   openDialog,
-  openDialogBootstrap,
-  openDialogMUI,
-  openDialogTailwindCSS,
-  openAboutSidebar,
-  getSheetsData,
-  addSheet,
-  deleteSheet,
-  setActiveSheet,
   getSelectedText,
   insertOrReplaceText,
 };
--- src/server/ui.js
+++ src/server/ui.js
@@ -10,31 +10,5 @@ export const openDialog = () => {
   const html = HtmlService.createHtmlOutputFromFile('dialog-demo')
     .setWidth(600)
     .setHeight(600);
-  DocumentApp.getUi().showModalDialog(html, 'Sheet Editor');
-};
-
-export const openDialogBootstrap = () => {
-  const html = HtmlService.createHtmlOutputFromFile('dialog-demo-bootstrap')
-    .setWidth(600)
-    .setHeight(600);
-  SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor (Bootstrap)');
-};
-
-export const openDialogMUI = () => {
-  const html = HtmlService.createHtmlOutputFromFile('dialog-demo-mui')
-    .setWidth(600)
-    .setHeight(600);
-  SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor (MUI)');
-};
-
-export const openDialogTailwindCSS = () => {
-  const html = HtmlService.createHtmlOutputFromFile('dialog-demo-tailwindcss')
-    .setWidth(600)
-    .setHeight(600);
-  SpreadsheetApp.getUi().showModalDialog(html, 'Sheet Editor (Tailwind CSS)');
-};
-
-export const openAboutSidebar = () => {
-  const html = HtmlService.createHtmlOutputFromFile('sidebar-about-page');
-  SpreadsheetApp.getUi().showSidebar(html);
+  DocumentApp.getUi().showModalDialog(html, 'Code Editor');
 };
