58971db7bb9b3dff6cc893fb6d76db629be2622a
Dennis Peng <dennp333@gmail.com>
2023-04-26 23:50:20 -0400

    more cleanup

--- jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  globalSetup: './test/global-setup.js',
-  globalTeardown: './test/global-teardown.js',
-  testEnvironment: './test/puppeteer-environment.js',
-  reporters: ['default', '<rootDir>/test/utils/image-reporter.js'],
-};
