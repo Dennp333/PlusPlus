3ecbb86e5c67416ba856d328fe0bc3c8b1885cbc
Dennis Peng <dennp333@gmail.com>
2023-05-01 18:07:27 -0400

    switch to monaco

--- src/client/app/components/App.jsx
+++ src/client/app/components/App.jsx
@@ -1,18 +1,14 @@
 import React, { useState, useEffect } from 'react'
-import Editor from 'react-simple-code-editor'
-import { highlight, languages } from 'prismjs/components/prism-core'
-import 'prismjs/components/prism-clike'
-import 'prismjs/components/prism-javascript'
-import 'prismjs/themes/prism.css' //Example style, you can use another
+import Editor from '@monaco-editor/react';
 import { serverFunctions } from '../../utils/serverFunctions'
 
 const App = () => {
-  const [text, setText] = useState([]);
+  const [code, setCode] = useState("");
 
   useEffect(async () => {
     try {
       const response = await serverFunctions.getSelectedText()
-      setText(response)
+      setCode(response)
     } catch (error) {
       alert(error)
     }
@@ -20,26 +16,22 @@ const App = () => {
 
   const replaceText = async () => {
     try {
-      const response = await serverFunctions.insertOrReplaceText(text)
+      const response = await serverFunctions.insertOrReplaceText(code)
     } catch (error) {
       alert(error)
     }
   }
 
   return (
-    <div>
+    <>
       <Editor
-        value={text}
-        onValueChange={text => setText(text)}
-        highlight={code => highlight(code, languages.js)}
-        padding={10}
-        style={{
-          fontFamily: '"Fira code", "Fira Mono", monospace',
-          fontSize: 12,
-        }}
+        value = {code}
+        onChange = {(value, event) => setCode(value)}
+        language = "python"
+        height = "400px"
       />
       <button onClick = {replaceText}>Done</button>
-    </div>
+    </>
   );
 };
 
